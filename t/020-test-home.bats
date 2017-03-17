07c94ce4340cf75589c42cbde5ed40699c517c0c
Nishant Rodrigues <nishantjr@gmail.com>
2017-03-17 10:36:35 +0900

    test-lib: New "test homedir" test framework
    This gives us functions to up a new, empty "home" dir with specific
    files and links in it, run `dot-home-setup` on that dir, and check the
    resulting file tree, nicely printing any differences.
    
    We split the printing of the diff and the assertion that there was no
    difference so that we can do other assertions in between. This means
    that when `assert_ouptut` fails we can see both the ouptut diff and
    the file tree diff.

---
+++ t/020-test-home.bats
@@ -0,0 +1,23 @@
+load 'test-lib'
+
+@test "test test_home" {
+    create_test_home <<_
+        # Blank lines and comment-only lines are ignored
+
+        .home/AAA/a file                # automatically added to expected
+        .home/,build                    # not automatically added
+        .homey                          # not in .home
+        another file
+        a symlink -> nothing at all     # test this comment :-)
+_
+    # run_setup_on_test_home
+    diff_test_home_with <<_
+        # Order does not matter here
+        another file
+        a symlink -> nothing at all
+        .homey
+        .home/,build
+_
+    assert_output ''
+    assert_test_home
+}
--- t/test-lib.bash
+++ t/test-lib.bash
@@ -25,3 +25,60 @@ setup_test_suite_home() {
     mkdir -p "$test_scratch_dir"
     cp -R "$test_suite_path.home" "$test_home"
 }
+
+create_test_home() {
+    test_home="$test_scratch_dir/home"
+    mkdir -p "$test_home"
+    home_expected="$test_scratch_dir/home.expected"
+
+    while read path; do
+        # Short-cut to include "non-build/install" .home files in expected
+        # output so we don't have to do so manually in the expected output
+        # in the tests.
+        [[ "$path" =~ ^.home/[A-Za-z0-9] ]] && echo "$path" >> "$home_expected"
+
+        if [[ "$path" =~ ' -> ' ]]; then
+            local target="${path##* -> }"
+            local path="${path%% -> *}"
+        fi
+
+        local abs_path="$test_home/$path"
+        mkdir -p "$(dirname "$abs_path")"
+
+        if [ -v target ]; then
+            ln -s "$target" "$abs_path"
+        else
+            echo "$path" > "$abs_path"
+        fi
+    done < <(sed -e 's/#.*//' -e '/^ *$/d')
+}
+
+run_setup_on_test_home() {
+    HOME="$test_home" run "$base_dir/bin/dot-home-setup"
+}
+
+diff_test_home_with() {
+    local home_actual="$test_scratch_dir/home.actual"
+    (cd $test_home && find . \
+               -type l  -exec bash -c 'echo -n {} "-> "; readlink "{}"' \; \
+            -o -type d \! -empty -true \
+            -o          -print \
+        | sed -e 's,^\./,,' | sort >"$home_actual")
+
+    sed -e 's/#.*//' -e 's/^ *//' -e '/^$/d' >> "$home_expected"
+    sort -o "$home_expected" "$home_expected"
+
+    test_home_diffed_ok=true
+    diff --suppress-common-lines -u \
+        --label expected "$home_expected" \
+        --label actual "$home_actual" || test_home_diffed_ok=false
+}
+
+# We separate the diff and the assertion so that we can display the
+# diff without (yet) failing, so that other assertions can also
+# display their results.
+#
+assert_test_home() {
+    $test_home_diffed_ok
+}
+
